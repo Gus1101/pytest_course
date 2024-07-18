ec569c2ee6dc6841838e13cf843d59762a6eb64d
Gus1101 <augustosantosb60@gmail.com>
2024-07-18 15:46:24 -0300

    primeiro teste sample

---
+++ tests/test_soma.py
@@ -0,0 +1,16 @@
+import pytest
+
+def test_soma():
+    """
+    Função para testar a soma entre dois valores inteiros
+    """
+    
+    # Arrange
+    valor1 = 10
+    valor2 = 20
+
+    # Act
+    x = sum([valor1,valor2])
+
+    # Assert
+    assert x == 30
